6cf007ad0c99570361444c8b69a82ec6fbad962c
Bianca Balabenute <balabenute.bianca@gmail.com>
2024-06-24 18:24:44 -0300

    criando post e get de usuarios

--- demo-park-api/src/main/java/com/balabenute/demo_park_api/service/UsuarioService.java
+++ demo-park-api/src/main/java/com/balabenute/demo_park_api/service/UsuarioService.java
@@ -1,12 +1,26 @@
 package com.balabenute.demo_park_api.service;
 
+import com.balabenute.demo_park_api.entity.Usuario;
 import com.balabenute.demo_park_api.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
+
+    @Transactional
+    public Usuario salvar(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    @Transactional(readOnly = true)
+    public Usuario buscarPorId(Long id) {
+        return usuarioRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("Usuario não encontrado.")
+        );
+    }
 }
--- demo-park-api/src/main/java/com/balabenute/demo_park_api/web/controller/UsuarioController.java
+++ demo-park-api/src/main/java/com/balabenute/demo_park_api/web/controller/UsuarioController.java
@@ -1,9 +1,12 @@
 package com.balabenute.demo_park_api.web.controller;
 
+import com.balabenute.demo_park_api.entity.Usuario;
 import com.balabenute.demo_park_api.service.UsuarioService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -11,4 +14,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
+
+    @PostMapping
+    public ResponseEntity<Usuario> create(@RequestBody Usuario usuario) {
+        Usuario user = usuarioService.salvar(usuario);
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> getById(@PathVariable Long id) {
+        Usuario user = usuarioService.buscarPorId(id);
+        return ResponseEntity.ok(user);
+    }
 }

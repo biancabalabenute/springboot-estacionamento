8c299388dbe8933aa0033cc0aa21ddf56b35b91b
Bianca Balabenute <balabenute.bianca@gmail.com>
2024-06-25 12:04:42 -0300

    patch e getAll de usuarios

--- demo-park-api/src/main/java/com/balabenute/demo_park_api/service/UsuarioService.java
+++ demo-park-api/src/main/java/com/balabenute/demo_park_api/service/UsuarioService.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Service
 public class UsuarioService {
@@ -22,5 +24,17 @@ public class UsuarioService {
         return usuarioRepository.findById(id).orElseThrow(
                 () -> new RuntimeException("Usuario não encontrado.")
         );
+    }
+
+    @Transactional
+    public Usuario editarSenha(Long id, String password) {
+        Usuario user = buscarPorId(id);
+        user.setPassword(password);
+        return user;
+    }
+
+    @Transactional(readOnly = true)
+    public List<Usuario> buscarTodos() {
+        return usuarioRepository.findAll();
     }
 }
--- demo-park-api/src/main/java/com/balabenute/demo_park_api/web/controller/UsuarioController.java
+++ demo-park-api/src/main/java/com/balabenute/demo_park_api/web/controller/UsuarioController.java
@@ -2,11 +2,12 @@ package com.balabenute.demo_park_api.web.controller;
 
 import com.balabenute.demo_park_api.entity.Usuario;
 import com.balabenute.demo_park_api.service.UsuarioService;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -25,5 +26,17 @@ public class UsuarioController {
     public ResponseEntity<Usuario> getById(@PathVariable Long id) {
         Usuario user = usuarioService.buscarPorId(id);
         return ResponseEntity.ok(user);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Usuario> updatePassword(@PathVariable Long id, @RequestBody Usuario usuario) {
+        Usuario user = usuarioService.editarSenha(id, usuario.getPassword());
+        return ResponseEntity.ok(user);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Usuario>> getAll() {
+        List<Usuario> users = usuarioService.buscarTodos();
+        return ResponseEntity.ok(users);
     }
 }

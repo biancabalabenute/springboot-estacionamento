8b9d1012130878a86276eb11ae9c1b7e3f9c9f9e
Bianca Balabenute <balabenute.bianca@gmail.com>
2024-06-25 15:58:46 -0300

    contexto dto de usuario completo

---
+++ demo-park-api/src/main/java/com/balabenute/demo_park_api/web/dto/UsuarioCreateDto.java
@@ -0,0 +1,14 @@
+package com.balabenute.demo_park_api.web.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class UsuarioCreateDto {
+
+    private String username;
+    private String password;
+}

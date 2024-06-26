162fbea71a7e542bee8c10340b719eba0c4b199f
Bianca Balabenute <balabenute.bianca@gmail.com>
2024-06-26 13:55:14 -0300

    bean validation

--- demo-park-api/src/main/java/com/balabenute/demo_park_api/web/dto/UsuarioCreateDto.java
+++ demo-park-api/src/main/java/com/balabenute/demo_park_api/web/dto/UsuarioCreateDto.java
@@ -1,5 +1,8 @@
 package com.balabenute.demo_park_api.web.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
@@ -9,6 +12,10 @@ import lombok.*;
 @ToString
 public class UsuarioCreateDto {
 
+    @NotBlank
+    @Email(message = "Formato do e-mail invalido", regexp = "^[a-z0-9.+-]+@[a-z0-9.-]+\\.[a-z]{2,}$")
     private String username;
+    @NotBlank
+    @Size(min = 6, max = 6)
     private String password;
 }

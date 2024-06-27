1cb0b45c0d9a351f25c01920b0b339f3cfea947e
Bianca Balabenute <balabenute.bianca@gmail.com>
2024-06-27 15:16:09 -0300

    finalizando testes ate o momento

--- demo-park-api/src/test/resources/sql/usuarios/usuarios-insert.sql
+++ demo-park-api/src/test/resources/sql/usuarios/usuarios-insert.sql
@@ -1,3 +1,3 @@
-insert into USUARIOS (id, username, password, role) values(100, 'ana@email.com', '123345', 'ROLE_ADMIN');
-insert into USUARIOS (id, username, password, role) values(101, 'bob@email.com', '123345', 'ROLE_CLIENTE');
-insert into USUARIOS (id, username, password, role) values(102, 'jose@email.com', '123345', 'ROLE_CLIENTE');
+insert into USUARIOS (id, username, password, role) values(100, 'ana@email.com', '123456', 'ROLE_ADMIN');
+insert into USUARIOS (id, username, password, role) values(101, 'bob@email.com', '123456', 'ROLE_CLIENTE');
+insert into USUARIOS (id, username, password, role) values(102, 'jose@email.com', '123456', 'ROLE_CLIENTE');
